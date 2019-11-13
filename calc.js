59aa9a9f0c1add742ee9638482c98050289a40d1
Chris <51767118+AlgNinja@users.noreply.github.com>
2019-11-12 19:49:18 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,7 @@
 x=()=>a=prompt("1st #"),b=prompt("operator"),c=prompt("2nd #");b=="+"?:alert(a+b):b=="-"?alert(a-b):b=="x"||"*"?alert(a*b):b=="/"?alert(a/b):alert("error")
+
+//thats the main part dont count the rest :) now is just to get it to run
+
+window.onload = x;
+
+
