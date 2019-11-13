a646d8a1e228bb150acd74517588bcc393aebd2c
Chris <51767118+AlgNinja@users.noreply.github.com>
2019-11-12 20:26:53 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-x=()=>var a=window.prompt("1st#"),b=window.prompt("operator"),c=window.prompt("2nd#");b=="+"?:alert(a+c):b=="-"?alert(a-c):b=="x"||"*"?alert(a*c):b=="/"?alert(a/c):alert("error")
+x=()=>var a=window.prompt("1st#");var b=window.prompt("operator"); var c=window.prompt("2nd#");b=="+"?:alert(a+c):b=="-"?alert(a-c):b=="x"||"*"?alert(a*c):b=="/"?alert(a/c):alert("error")
 
 
 //thats the main part dont count the rest :) now is just to get it to run
